3af7061d184ec71d4f033b27a3affff94e28bf47
wiredmartian <solomzi.jikani@gmail.com>
2023-09-10 23:09:25 +0200

    wip: setting up

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      statements: 80
+    }
+  }
+};
