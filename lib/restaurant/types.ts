0868bf35a6f7a5ae64740b2b64e3c5433b2e9858
wiredmartian <solomzi.jikani@gmail.com>
2023-09-12 23:12:02 +0200

    wip: basic folder setup

---
+++ lib/restaurant/types.ts
@@ -0,0 +1,22 @@
+import { DateMetadata, Province } from "../types"
+
+type Restaurant = {
+    restaurantId: string
+    name: string
+    summary?: string
+    distance?: number
+    imageUrl: string
+    address: Address
+} & DateMetadata
+
+type Address = {
+    addressId: string
+    line1: string
+    line2?: string
+    city: string
+    state: Province
+    /** South Africa */
+    country: string
+    latitude: number
+    longitude: number
+}
