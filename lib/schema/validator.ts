410ff37978ca1aefa528443f12bc82c628a7d456
wiredmartian <solomzi.jikani@gmail.com>
2023-09-24 13:58:05 +0200

    form file validator

--- lib/schema/validator.ts
+++ lib/schema/validator.ts
@@ -1,6 +1,9 @@
 import Ajv from "ajv"
+import { extname } from "path"
 
-export const validator = <T>(schema: Record<string, string>, data: T) => {
+const MB = Math.pow(2, 1024)
+
+const validate = <T>(schema: Record<string, string>, data: T) => {
     const validator = new Ajv({ allErrors: true }).compile(schema)
 
     const valid = validator(data)
@@ -9,4 +12,34 @@ export const validator = <T>(schema: Record<string, string>, data: T) => {
         if (valid) resolve(valid)
         reject(validator.errors)
     })
+}
+
+const formFileValid = (fieldValue: FormDataEntryValue) => {
+    if (typeof fieldValue === "string") {
+        return Promise.reject(Error("expected a File but received string"))
+    }
+    const file = fieldValue as File
+
+    if (
+        !["image/png", "image/jpeg", "image/jpg"].includes(file.type) ||
+        !["png", "jpeg", "jpg"].includes(extname(file.name))
+    ) {
+        return Promise.reject(Error("invalid file type"))
+    }
+
+    if (file.size > 1 * MB) {
+        return Promise.reject(Error("file too large. file must not exceed 1mb"))
+    }
+    return Promise.resolve()
+}
+
+export const validator = {
+    /**
+     * validates a Json schema and returns a rejected promise if invalid
+     */
+    validateJsonSchema: validate,
+    /**
+     * Validates file contents returns rejected promised if it fails some rules
+     */
+    validateFormFile: formFileValid
 }
