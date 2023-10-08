47bb7b9acffc6e48768cc8172c4b1f4e5c1d3f96
wiredmartian <solomzi.jikani@gmail.com>
2023-10-08 13:11:17 +0200

    added menu item and group class in API bootstrap

--- lib/whatsamenu.ts
+++ lib/whatsamenu.ts
@@ -4,6 +4,8 @@ import { Menu } from "./menu"
 import { Allergen } from "./allergen"
 import { Ingredient } from "./ingredient"
 import { Auth } from "./auth"
+import { MenuItem } from "./menuitem"
+import { MenuGroup } from "./menugroup"
 
 export class MenuAPI {
     private http: AxiosInstance
@@ -17,8 +19,8 @@ export class MenuAPI {
             allergen: new Allergen(this.http),
             ingredient: new Ingredient(this.http),
             auth: new Auth(this.http),
-            menuItem: null,
-            menuGroup: null
+            menuItem: new MenuItem(this.http),
+            menuGroup: new MenuGroup(this.http)
         }
     }
 }
