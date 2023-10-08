b536a7aa7dc788b1e09f139396a30b253175a940
wiredmartian <solomzi.jikani@gmail.com>
2023-10-08 10:20:37 +0200

    (feat): describing menu item api

--- lib/menuitem/menuitem.ts
+++ lib/menuitem/menuitem.ts
@@ -1,5 +1,9 @@
+import { AxiosInstance } from "axios"
 import { IngredientResult } from "../ingredient"
-import { DateMetadata } from "../types"
+import { DateMetadata, ResponseMessage } from "../types"
+import { createMenuItemSchema, validator } from "../schema"
+import { CreateMenuItemInput } from "../schema/menu-item-schema"
+import { AllergenResult } from "../allergen"
 
 export type MenuItemResult = {
     /** menu item id */
@@ -21,3 +25,93 @@ export type MenuItemResult = {
     /** ingredients used to make the item */
     ingredients: Array<IngredientResult> | null
 } & DateMetadata
+
+export class MenuItem {
+    private client: AxiosInstance
+
+    constructor(httpClient: AxiosInstance) {
+        this.client = httpClient
+    }
+
+    /**
+     * Gets a menu item by id
+     * @param menuItemId - menu item id
+     * @returns a menu item
+     */
+    async getMenuItem(id: number): Promise<MenuItemResult> {
+        return this.client
+            .get<MenuItemResult>(`/menu-item/${id}`)
+            .then((response) => response.data)
+    }
+
+    /**
+     * Updates a menu group's menu item
+     * @param id - menu item id
+     * @param data - menu item update input
+     * @returns a response message
+     */
+    async update(
+        id: number,
+        data: CreateMenuItemInput
+    ): Promise<ResponseMessage> {
+        return validator
+            .validateJsonSchema(createMenuItemSchema, data)
+            .then(() =>
+                this.client
+                    .put<ResponseMessage>(`/menu-item/${id}`, data)
+                    .then((response) => response.data)
+            )
+    }
+
+    /**
+     * Marks a menu item for deletion
+     * @param id - menu item id
+     * @returns a response message
+     */
+    async delete(id: number): Promise<ResponseMessage> {
+        return this.client
+            .delete<ResponseMessage>(`/menu-item/${id}`)
+            .then((response) => response.data)
+    }
+
+    /**
+     * Adds an allergen to menu item
+     * @param id - menu item id
+     * @param data - allergen input
+     * @returns response message
+     * @example const result = await addAllergen(3 { allergenId: "54" })
+     */
+    async addAllergen(
+        id: number,
+        data: { allergenId: string }
+    ): Promise<ResponseMessage> {
+        return this.client
+            .post<ResponseMessage>(`/menu-item/${id}/allergens`, data)
+            .then((response) => response.data)
+    }
+
+    /**
+     * Gets a list of all menu item allergens
+     * @param id - menu item id
+     * @returns a list of allergens
+     */
+    async getAllergens(id: number): Promise<AllergenResult[]> {
+        return this.client
+            .get<AllergenResult[]>(`/menu-item/${id}/allergens`)
+            .then((response) => response.data)
+    }
+
+    /**
+     * Deletes a menu item allergen
+     * @param menuItemId - menu item id
+     * @param allergenId - menu item allergen id
+     * @returns response message
+     */
+    async deleteAllergen(menuItemId: number, allergenId: number) {
+        return this.client
+            .delete<ResponseMessage>(
+                `/menu-item/${menuItemId}/allergens/${allergenId}`
+            )
+            .then((response) => response.data)
+    }
+}
